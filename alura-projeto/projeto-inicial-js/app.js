c5c177746097293a34a917d63ce69d55d4dc850a
Ingrid Bini <binifideli@gmail.com>
2025-07-08 20:42:18 -0300

    first commit

---
+++ alura-projeto/projeto-inicial-js/app.js
@@ -0,0 +1,22 @@
+alert("Boas vindas ao nosso site!");
+
+var nome = "Lua";
+var idade = 25;
+var numeroDeVendas = 50;
+var saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos");
+
+var mensagemDeErro = "Erro! Preencha todos os campos"
+alert(mensagemDeErro);
+
+var nome2 = prompt("Qual seu nome?");
+var idade2 = prompt("Qual a sua idade?");
+
+var idade3 = 18;
+
+if (idade3 < 18) {
+    console.log("Menor de idade")
+} else (
+    console.log("Pode tirar a habilitação!")
+)
