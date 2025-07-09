3a7106528dd84e3cc7d1567df14b4a40133fcfe7
Ingrid Bini <binifideli@gmail.com>
2025-07-08 22:49:19 -0300

    js-atualixado-finalizado

--- alura-projeto/projeto-inicial-js/app.js
+++ alura-projeto/projeto-inicial-js/app.js
@@ -1,14 +1,15 @@
 alert ("Boas vindas ao jogo do número secreto");
 
-let numeroSecreto = 5;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let  chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 1 e 10");
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     if (chute == numeroSecreto) {
-        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -18,3 +19,15 @@ while (chute != numeroSecreto) {
         tentativas ++;
     }
 }
+// Operador ternário
+//         tentativas é maior q 1?   sim "tentativas" não: "tentativa"
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+/*
+if (tentativas > 1){
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}
+*/
